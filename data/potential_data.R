96e2f51a1829e2beb43da2ef80850a8ee78b39e9
Stephanie Pusker <stephmp13@gmail.com>
2022-11-16 10:37:52 -0700

    R script with data in it

---
+++ data/potential_data.R
@@ -0,0 +1,9 @@
+## let's create a new project!
+
+library(tidyverse)
+
+nyc_squirrels <- read_csv("data/nyc_squirrels.csv")
+
+ramen_ratings <- read_csv("data/ramen_ratings.csv")
+
+emperors <- read_csv("data/emperors.csv")
